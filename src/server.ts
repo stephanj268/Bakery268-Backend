7a3f292b7440688fc2c87a12ac6762f5dc8b48eb
LUHCaprisun <travoughna1@gmail.com>
2026-05-27 22:31:24 -0400

    xz

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express, { type Request, type Response } from 'express';
+import { database } from '../config/database';
+import mongoose from 'mongoose';
 import cors from 'cors';
 
 const app = express();
@@ -9,8 +11,15 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+mongoose.connect(database).then(() => {
+    console.log(`[Connection to DATABASE] - ${database} CONNECTED 🥖`);
+
+}).catch((err) => {
+    console.log(err);
+});
+
 app.get('/', (req: Request, res: Response) => {
-    res.send('cdcsd');
+    res.send('Server Running');
 
 });
 
