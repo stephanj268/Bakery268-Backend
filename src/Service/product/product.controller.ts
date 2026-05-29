57d5050064a3e9157264df0755efb9905f552e2e
LUHCaprisun <travoughna1@gmail.com>
2026-05-28 20:52:05 -0400

    scd csd

--- src/Service/product/product.controller.ts
+++ src/Service/product/product.controller.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { getProduct, createProduct, getProductById, updateProduct } from './product.service';
+import { getProduct, createProduct, getProductById, updateProduct, deleteProduct } from './product.service';
 
 const router = express.Router()
 
@@ -18,24 +18,31 @@ function slice(val: any) {
 
 router.post('/', (req: Request, res: Response) => {
     createProduct(req, res)
-})
+});
 
 router.get('/', (req: Request, res: Response) => {
     getProduct(req, res);
-})
+});
 
 router.get('/:id', (req: Request, res: Response) => {
     let id = req.params.id
     id = slice(id);
 
     getProductById(id, req, res);
-})
+});
 
 router.put('/:id', (req: Request, res: Response) => {
     let id = req.params.id
     id = slice(id);
 
     updateProduct(id, req, res);
-})
+});
+
+router.delete('/:id', (req: Request, res: Response) => {
+    let id = req.params.id
+    id = slice(id);
+
+    deleteProduct(id, req, res);
+});
 
 export { router as UserRouter }
