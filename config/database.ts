139e13d0bd6410e931a636896844527e3bc9a8e3
LUHCaprisun <travoughna1@gmail.com>
2026-05-28 22:17:06 -0400

    as as

--- config/database.ts
+++ config/database.ts
@@ -1,1 +1,2 @@
-export const database = 'mongodb://localhost:27017/Bakery'
+//export const database = 'mongodb://localhost:27017/Bakery'
+export const database = 'mongodb+srv://bakery:admin@bakery.yrzbd8q.mongodb.net/'
